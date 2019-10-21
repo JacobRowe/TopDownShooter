cd408473fd949ef666daf7167299df51f7ef3293
JacobRowe <55706787+JacobRowe@users.noreply.github.com>
2019-10-21 15:44:01 +0100

    Added Basic Scripts
    Added:
    - Mouse Smooth Look script
    - Smooth Follow script
    - Top Down Character

--- Top Down Shooter Project/Assets/Scripts/TopDownCharacterController2D.cs
+++ Top Down Shooter Project/Assets/Scripts/TopDownCharacterController2D.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class TopDownCharacterController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
     }
 }
