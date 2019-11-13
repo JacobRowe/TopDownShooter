b979cdbdb67257696eb170a509a41a54a33fa6c3
JacobRowe <55706787+JacobRowe@users.noreply.github.com>
2019-11-13 15:53:59 +0000

    Added UI script and assets
    - Added buttons for pause menu
    - Started UI Manager script

---
+++ Top Down Shooter Project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    GameObject[] pauseUIObjects;
+
+    void Start()
+    {
+        Time.timescale = 1;
+        pauseUIObjects = 
+    }
+
+   
+}
